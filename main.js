a13c701aa01acf920428e9edfa7c41c7f9b51410
Ashish Prasad <56514722+1-Mirage@users.noreply.github.com>
2021-10-21 13:22:10 +0530

    Update bonus task

--- main.js
+++ main.js
@@ -7,8 +7,17 @@ var outputBox = document.getElementById("output-box");
 
 
 
-
+function reloadpage()
+{
+  location.reload();
+}
 function calculate() {
+  if(initialPrice.value===""||stocksQuantity.value===""||currentPrice.value==="")
+  {
+    reloadpage();
+    alert("Please fill out all Fields");
+  }
+
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
@@ -17,14 +26,17 @@ function calculate() {
   if(ip<curr)
   {
     let profit=(curr-ip)*qty;
-    let profitpercent=(profit/ip)*100;
-    message=`The profit is ${Math.abs(profit)} and the percent is ${Math.abs(profitpercent)}%`;
+    let profitpercent=(profit/ip)*100.0;
+    message=`The profit is ${Math.abs(profit)} and the percent is ${Math.abs(profitpercent.toFixed(2))}%`;
+    outputBox.style.color='green'
   }
   else if(ip>curr)
   {
     let loss=(ip-curr)*qty;
-    let losspercent=(loss/ip)*100;
-    message=`The loss is ${Math.abs(loss)} and the percent is ${Math.abs(losspercent)}%`
+    let losspercent=(loss/ip)*100.0;
+    
+    message=`The loss is ${Math.abs(loss)} and the percent is ${Math.abs(losspercent.toFixed(2))}%`;
+    outputBox.style.color='red'
   }
   else
   {
